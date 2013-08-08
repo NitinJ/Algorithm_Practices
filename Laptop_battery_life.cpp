191498d8fd49c23d3171ce9c30646d5226ed5951
Jayram Kumar <jayram.chandan@gmail.com>
2013-08-08 17:02:02 +0530

    Added laptop battery life

---
+++ Laptop_battery_life.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    float a;
+    cin>>a;
+    if(a<4.00)
+        cout<<a*2<<endl;
+    else
+        cout<<"8"<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
